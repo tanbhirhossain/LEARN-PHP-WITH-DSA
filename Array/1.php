8b5685240cd7b3a983d52f5bc37ee0877eb90716
unknown <tanbhirhossaincs@gmail.com>
2025-02-11 15:30:15 +0600

    Array Sort

---
+++ Array/1.php
@@ -0,0 +1,13 @@
+<?php 
+
+$arr = [0,1,2,3,4,5,6,7];
+
+var_dump($arr);
+
+array_push($arr, 8);
+
+var_dump($arr);
+
+array_pop($arr);
+
+var_dump($arr);
---
+++ Array/Sorting.php
@@ -0,0 +1,10 @@
+<?php 
+
+$arr1 = [3,4,7,1,2,9,6,5,8];
+
+var_dump($arr1);
+
+sort($arr1);
+
+
+var_dump($arr1);
